34b976bdb3de20838ec2a95bad555a7c614538a1
Aleksandr Grigorii <domosedov.dev@gmail.com>
2020-11-17 17:49:26 +0300

    handle POST profile route

--- src/Routes/Profile.php
+++ src/Routes/Profile.php
@@ -29,150 +29,158 @@ class Profile extends WP_REST_Controller {
 
 	public function getCreateProfileArgs() {
 		return [
-			'firstName'  => [
-				'type'        => 'string',
-				'description' => 'Имя',
-				'default'     => '',
-				'required'    => true,
-				'sanitize_callback' => [$this, 'sanitizeArgs'],
-				'validate_callback' => [$this, 'validateArgs']
+			'firstName'   => [
+				'type'              => 'string',
+				'description'       => 'Имя',
+				'default'           => '',
+				'required'          => true,
+				'sanitize_callback' => [ $this, 'sanitizeArgs' ],
+				'validate_callback' => [ $this, 'validateArgs' ]
 			],
-			'middleName' => [
-				'type'        => 'string',
-				'description' => 'Отчество',
-				'default'     => '',
-				'required'    => true,
-				'sanitize_callback' => [$this, 'sanitizeArgs'],
-				'validate_callback' => [$this, 'validateArgs']
+			'middleName'  => [
+				'type'              => 'string',
+				'description'       => 'Отчество',
+				'default'           => '',
+				'required'          => true,
+				'sanitize_callback' => [ $this, 'sanitizeArgs' ],
+				'validate_callback' => [ $this, 'validateArgs' ]
 			],
-			'lastName'  => [
-				'type'        => 'string',
-				'description' => 'Фамилия',
-				'default'     => '',
-				'required'    => true,
-				'sanitize_callback' => [$this, 'sanitizeArgs'],
-				'validate_callback' => [$this, 'validateArgs']
+			'lastName'    => [
+				'type'              => 'string',
+				'description'       => 'Фамилия',
+				'default'           => '',
+				'required'          => true,
+				'sanitize_callback' => [ $this, 'sanitizeArgs' ],
+				'validate_callback' => [ $this, 'validateArgs' ]
 			],
-			'phone' => [
-				'type'        => 'string',
-				'description' => 'Телефон',
-				'default'     => '',
-				'required'    => true,
-				'sanitize_callback' => [$this, 'sanitizeArgs'],
-				'validate_callback' => [$this, 'validateArgs']
+			'phone'       => [
+				'type'              => 'string',
+				'description'       => 'Телефон',
+				'default'           => '',
+				'required'          => true,
+				'sanitize_callback' => [ $this, 'sanitizeArgs' ],
+				'validate_callback' => [ $this, 'validateArgs' ]
 			],
-			'email'  => [
-				'type'        => 'string',
-				'description' => 'Email',
-				'default'     => '',
-				'required'    => true,
-				'sanitize_callback' => [$this, 'sanitizeArgs'],
-				'validate_callback' => [$this, 'validateArgs']
+			'email'       => [
+				'type'              => 'string',
+				'description'       => 'Email',
+				'default'           => '',
+				'required'          => true,
+				'sanitize_callback' => [ $this, 'sanitizeArgs' ],
+				'validate_callback' => [ $this, 'validateArgs' ]
 			],
-			'education' => [
-				'type'        => 'string',
-				'description' => 'Образование',
-				'default'     => '',
-				'required'    => true,
-				'sanitize_callback' => [$this, 'sanitizeArgs'],
-				'validate_callback' => [$this, 'validateArgs']
+			'education'   => [
+				'type'              => 'string',
+				'description'       => 'Образование',
+				'default'           => '',
+				'required'          => true,
+				'sanitize_callback' => [ $this, 'sanitizeArgs' ],
+				'validate_callback' => [ $this, 'validateArgs' ]
 			],
-			'birthYear'  => [
-				'type'        => 'integer',
-				'description' => 'Год рождения',
-				'default'     => 0,
-				'required'    => true,
-				'sanitize_callback' => [$this, 'sanitizeArgs'],
-				'validate_callback' => [$this, 'validateArgs']
+			'birthYear'   => [
+				'type'              => 'integer',
+				'description'       => 'Год рождения',
+				'default'           => 0,
+				'required'          => true,
+				'sanitize_callback' => [ $this, 'sanitizeArgs' ],
+				'validate_callback' => [ $this, 'validateArgs' ]
 			],
-			'hourlyRate' => [
-				'type'        => 'integer',
-				'description' => 'Ставка в час',
-				'default'     => 0,
-				'required'    => true,
-				'sanitize_callback' => [$this, 'sanitizeArgs'],
-				'validate_callback' => [$this, 'validateArgs']
+			'hourlyRate'  => [
+				'type'              => 'integer',
+				'description'       => 'Ставка в час',
+				'default'           => 0,
+				'required'          => true,
+				'sanitize_callback' => [ $this, 'sanitizeArgs' ],
+				'validate_callback' => [ $this, 'validateArgs' ]
 			],
 			'experience'  => [
-				'type'        => 'integer',
-				'description' => 'Год начала деятельности',
-				'default'     => 0,
-				'required'    => true,
-				'sanitize_callback' => [$this, 'sanitizeArgs'],
-				'validate_callback' => [$this, 'validateArgs']
+				'type'              => 'integer',
+				'description'       => 'Год начала деятельности',
+				'default'           => 0,
+				'required'          => true,
+				'sanitize_callback' => [ $this, 'sanitizeArgs' ],
+				'validate_callback' => [ $this, 'validateArgs' ]
 			],
-			'city' => [
-				'type'        => 'integer',
-				'description' => 'ID город',
-				'default'     => 0,
-				'required'    => true,
-				'sanitize_callback' => [$this, 'sanitizeArgs'],
-				'validate_callback' => [$this, 'validateArgs']
+			'city'        => [
+				'type'              => 'integer',
+				'description'       => 'ID город',
+				'default'           => 0,
+				'required'          => true,
+				'sanitize_callback' => [ $this, 'sanitizeArgs' ],
+				'validate_callback' => [ $this, 'validateArgs' ]
 			],
-			'status' => [
-				'type'        => 'integer',
-				'description' => 'ID Статус',
-				'default'     => 0,
-				'required'    => true,
-				'sanitize_callback' => [$this, 'sanitizeArgs'],
-				'validate_callback' => [$this, 'validateArgs']
+			'status'      => [
+				'type'              => 'integer',
+				'description'       => 'ID Статус',
+				'default'           => 0,
+				'required'          => true,
+				'sanitize_callback' => [ $this, 'sanitizeArgs' ],
+				'validate_callback' => [ $this, 'validateArgs' ]
 			],
-			'places'  => [
-				'type'        => 'array',
-				'items' => [
+			'gender'      => [
+				'type'              => 'integer',
+				'description'       => 'ID Пол',
+				'default'           => 0,
+				'required'          => true,
+				'sanitize_callback' => [ $this, 'sanitizeArgs' ],
+				'validate_callback' => [ $this, 'validateArgs' ]
+			],
+			'place'      => [
+				'type'              => 'array',
+				'items'             => [
 					'type' => 'integer'
 				],
-				'description' => 'Ids Места занятий',
-				'default'     => [],
-				'required'    => true,
-				'sanitize_callback' => [$this, 'sanitizeArgs'],
-				'validate_callback' => [$this, 'validateArgs']
+				'description'       => 'Ids Места занятий',
+				'default'           => [],
+				'required'          => true,
+				'sanitize_callback' => [ $this, 'sanitizeArgs' ],
+				'validate_callback' => [ $this, 'validateArgs' ]
 			],
-			'subjects'  => [
-				'type'        => 'array',
-				'items' => [
+			'subject'    => [
+				'type'              => 'array',
+				'items'             => [
 					'type' => 'integer'
 				],
-				'description' => 'IDs Предметы',
-				'default'     => [],
-				'required'    => true,
-				'sanitize_callback' => [$this, 'sanitizeArgs'],
-				'validate_callback' => [$this, 'validateArgs']
+				'description'       => 'IDs Предметы',
+				'default'           => [],
+				'required'          => true,
+				'sanitize_callback' => [ $this, 'sanitizeArgs' ],
+				'validate_callback' => [ $this, 'validateArgs' ]
 			],
-			'students'  => [
-				'type'        => 'array',
-				'items' => [
+			'student'    => [
+				'type'              => 'array',
+				'items'             => [
 					'type' => 'integer'
 				],
-				'description' => 'IDs Категории учеников',
-				'default'     => [],
-				'required'    => true,
-				'sanitize_callback' => [$this, 'sanitizeArgs'],
-				'validate_callback' => [$this, 'validateArgs']
+				'description'       => 'IDs Категории учеников',
+				'default'           => [],
+				'required'          => true,
+				'sanitize_callback' => [ $this, 'sanitizeArgs' ],
+				'validate_callback' => [ $this, 'validateArgs' ]
 			],
-			'metro' => [
-				'type'        => 'integer',
-				'description' => 'ID Метро',
-				'default'     => 0,
-				'required'    => false,
-				'sanitize_callback' => [$this, 'sanitizeArgs'],
-				'validate_callback' => [$this, 'validateArgs']
+			'metro'       => [
+				'type'              => 'integer',
+				'description'       => 'ID Метро',
+				'default'           => 0,
+				'required'          => false,
+				'sanitize_callback' => [ $this, 'sanitizeArgs' ],
+				'validate_callback' => [ $this, 'validateArgs' ]
 			],
-			'area' => [
-				'type'        => 'string',
-				'description' => 'Район',
-				'default'     => '',
-				'required'    => false,
-				'sanitize_callback' => [$this, 'sanitizeArgs'],
-				'validate_callback' => [$this, 'validateArgs']
+			'area'        => [
+				'type'              => 'string',
+				'description'       => 'Район',
+				'default'           => '',
+				'required'          => false,
+				'sanitize_callback' => [ $this, 'sanitizeArgs' ],
+				'validate_callback' => [ $this, 'validateArgs' ]
 			],
 			'description' => [
-				'type'        => 'string',
-				'description' => 'Дополнительно',
-				'default'     => '',
-				'required'    => false,
-				'sanitize_callback' => [$this, 'sanitizeArgs'],
-				'validate_callback' => [$this, 'validateArgs']
+				'type'              => 'string',
+				'description'       => 'Дополнительно',
+				'default'           => '',
+				'required'          => false,
+				'sanitize_callback' => [ $this, 'sanitizeArgs' ],
+				'validate_callback' => [ $this, 'validateArgs' ]
 			],
 		];
 	}
@@ -186,12 +194,15 @@ class Profile extends WP_REST_Controller {
 
 		$firstName = $request->get_param( 'firstName' );
 		$avatar    = $request->get_file_params()['avatar'];
+		$docs    = $request->get_file_params()['documents'];
+
+		$students = $request->get_param( 'student' );
 
 		if ( empty( $avatar['error'] ) ) {
 			$uploadedImageId = handleUploadImageFile( $avatar, 20 );
 		}
 
-		return rest_ensure_response( [ 'message' => $uploadedImageId ?? false, 'user' => $firstName ] );
+		return rest_ensure_response( $request->get_params() );
 	}
 
 	/**
@@ -207,19 +218,129 @@ class Profile extends WP_REST_Controller {
 	 * @param mixed $value
 	 * @param WP_REST_Request $request
 	 * @param string $param
+	 *
 	 * @return mixed
 	 */
-	public function sanitizeArgs($value, $request, $param) {
-		return $value;
+	public function sanitizeArgs( $value, $request, $param ) {
+		switch ( $param ) {
+			case 'firstName':
+			case 'middleName':
+			case 'lastName':
+			case 'area':
+			case 'phone':
+				return sanitize_text_field( $value );
+			case 'email':
+				return sanitize_email( $value );
+			case 'education':
+			case 'description':
+				return sanitize_textarea_field( $value );
+			case 'birthYear':
+			case 'hourlyRate':
+			case 'experience':
+			case 'metro':
+			case 'city':
+			case 'status':
+			case 'gender':
+				return absint( $value );
+			case 'student':
+			case 'subject':
+			case 'place':
+				return $this->sanitizeArrayOfInteger( $value );
+			default:
+				return $value;
+		}
+	}
+
+	/**
+	 * @param array $arr
+	 *
+	 * @return int[] Возвращает массив целых чисел или пустой массив
+	 */
+	public function sanitizeArrayOfInteger( $arr ) {
+		if ( ! is_array( $arr ) ) {
+			return [];
+		}
+
+		if ( empty( $arr ) ) {
+			return [];
+		}
+
+		$output = array_filter( array_map( function ( $item ) {
+			return is_numeric( $item ) ? absint( $item ) : 0;
+		}, $arr ), function ( $item ) {
+			return $item > 0;
+		} );
+
+		return ( count( $output ) === count( $arr ) ) ? $output : [];
 	}
 
 	/**
 	 * @param mixed $value
 	 * @param WP_REST_Request $request
 	 * @param string $param
+	 *
 	 * @return true|WP_Error
 	 */
-	public function validateArgs($value, $request, $param) {
+	public function validateArgs( $value, $request, $param ) {
+		switch ( $param ) {
+			case 'firstName':
+			case 'middleName':
+			case 'lastName':
+			case 'education':
+			case 'phone':
+				return is_string( $value ) && ! empty( $value );
+			case 'email':
+				return is_email( $value ) && ! empty( $value );
+			case 'birthYear':
+			case 'hourlyRate':
+			case 'experience':
+				return ! empty( absint( $value ) );
+			case 'city':
+			case 'status':
+			case 'gender':
+				return $this->isValidTermId( $value, $param );
+			case 'metro':
+				return empty(absint($value)) ? true : $this->isValidTermId( $value, $param );
+			case 'student':
+			case 'subject':
+			case 'place':
+				return $this->isValidTermIds( $value, $param );
+			default:
+				return true;
+		}
+	}
+
+	/**
+	 * @param string|int $value Argument value
+	 * @param string $param Argument key
+	 *
+	 * @return bool
+	 */
+	public function isValidTermId( $value, $param ) {
+		return array_search( absint( $value ), get_terms( [
+				'fields'     => 'ids',
+				'hide_empty' => false,
+				'taxonomy'   => EXCELLENT_EXAM_CORE_PREFIX . $param
+			] ) ) !== false;
+	}
+
+	public function isValidTermIds( $values, $param ) {
+		if (!is_array($values) || empty($values)) {
+			return false;
+		}
+
+		$termIds = get_terms([
+			'fields'     => 'ids',
+			'hide_empty' => false,
+			'taxonomy'   => EXCELLENT_EXAM_CORE_PREFIX . $param
+		]);
+
+		foreach ($values as $value) {
+			if (array_search(absint($value), $termIds) === false) {
+				return false;
+			}
+		}
+
 		return true;
 	}
 }
